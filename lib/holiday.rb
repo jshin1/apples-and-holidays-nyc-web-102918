f0675c5e59a3079afd276c88bd875399b614eeee
Ji S <j2yshin@gmail.com>
2018-10-18 18:15:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,9 +68,7 @@ def all_winter_holiday_supplies(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     if season == :winter
-      winter_supplies = holiday.collect do |name, value|
-        value
-      end
+      holiday_hash[season].values.flatten
     end
   end
 end
