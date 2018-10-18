275d9845da6074d560b314e28fc4fdcc1318809e
Ji S <j2yshin@gmail.com>
2018-10-18 19:16:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -97,6 +97,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
+  holidays_with_bbq = []
+  
   
   
   
